4c6e6543099b1bd6e7976366ca0e22ed53544804
Armando Ruiz <aranruiz13@gmail.com>
2023-10-24 16:16:45 -0600

    #29 Add hilt dependencies in MarvelFeaturePlugin as default dependencies (#30)
    * #29 Add hilt dependencies in MarvelFeaturePlugin as default dependencies for modules that implement this plugin
    
    * #29 Add hilt dependencies in MarvelFeaturePlugin as default dependencies for modules that implement this plugin

--- buildSrc/src/main/kotlin/com/rockar/android/marvelapp/plugins/MarvelFeaturePlugin.kt
+++ buildSrc/src/main/kotlin/com/rockar/android/marvelapp/plugins/MarvelFeaturePlugin.kt
@@ -1,19 +1,27 @@
 package com.rockar.android.marvelapp.plugins
 
 import com.rockar.android.marvelapp.dependencies.GradleConfigVersions
+import com.rockar.android.marvelapp.dependencies.HiltDependencies
 import com.rockar.android.marvelapp.dependencies.Plugins
 import com.rockar.android.marvelapp.dependencies.ProjectDependencies
 import com.rockar.android.marvelapp.utils.android
 import com.rockar.android.marvelapp.utils.kotlinOptions
+import com.rockar.android.marvelapp.utils.implementation
+import com.rockar.android.marvelapp.utils.kapt
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.get
 
 class MarvelFeaturePlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         configurePlugins(target)
         configureAndroidLibrary(target)
+        configureDependencies(target)
     }
 
     private fun configurePlugins(target: Project) {
@@ -54,6 +62,13 @@ class MarvelFeaturePlugin : Plugin<Project> {
             kotlinOptions {
                 jvmTarget = GradleConfigVersions.jvmTarget
             }
+        }
+    }
+
+    private fun configureDependencies(target: Project) {
+        target.dependencies {
+            implementation(HiltDependencies.hiltAndroid)
+            kapt(HiltDependencies.hiltCompiler)
         }
     }
 }
